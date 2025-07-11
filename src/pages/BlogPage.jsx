cfe7e4ef418982e48bd2107f03ff03785b76c774
Ridhima2323 <sridhima229@gmail.com>
2025-07-12 00:10:57 +0530

    Add: Blog page articles

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -4,47 +4,45 @@ const BlogPage = () => {
     const posts = [
         {
             title: "The Peace Lily: NASA's Little Air-Purifying Superstar",
-            date: "31 Mar 2021",
-            image: "https://via.placeholder.com/600x400?text=Garden+Plants",
-            desc: "Explore a curated list of 10 stunning garden plants—from roses to crotons—that fit any garden size.",
+            date: "September 1, 2023",
+            image: "https://plus.unsplash.com/premium_photo-1708769592969-9f42825496a7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGVhY2UlMjBsaWx5fGVufDB8fDB8fHww",
+            desc: "You've probably heard about the Peace Lily being crowned as the air-purifying champion of the plant world. But did you know it's not just your average houseplant? ",
             url: "https://nurserylive.com/blogs/top-10-plants/top-10-garden-plants-for-your-collection"
         },
         {
-            title: "Top 10 Must-have Fruit Plants For Your Garden",
-            date: "15 Mar 2021",
-            image: "https://via.placeholder.com/600x400?text=Fruit+Plants",
-            desc: "Grow your own fruits with these top 10 easy-to-grow fruit plants and their requirements.",
-            url: "https://nurserylive.com/blogs/top-10-plants/top-10-must-have-fruit-plants-for-your-garden"
+            title: "Meet Sanjeevani: A Plant That's as Legendary as Ramayana itself",
+            date: "August 26, 2023",
+            image: "https://cdn.shopify.com/s/files/1/0579/7924/0580/files/Selaginella_bryopteris_480x480.jpg?v=1687784522",
+            desc: "Ever heard of a plant so legendary, it's said to bring back the dead? No, we're not talking about a prop from a sci-fi movie. We're talking about the real deal—Sanjeevani, also known as Selaginella.",
+            url: "https://nurserylive.com/blogs/top-10-plants/meet-sanjeevani-a-plant-thats-as-legendary-as-ramayana-itself"
         },
         {
-            title: "Top 10 Highest Oxygen Producing Indoor Plants",
-            date: "25 Jan 2022",
-            image: "https://via.placeholder.com/600x400?text=Oxygen+Plants",
-            desc: "Discover indoor plants that generate the most oxygen and help purify your living spaces.",
-            url: "https://nurserylive.com/blogs/top-10-plants/top-10-highest-oxygen-producing-indoor-plants"
+            title: "Top 10 Fragrant Flowering Plants For Ultimate Garden Bliss",
+            date: "March 18, 2021 ",
+            image:"https://images.unsplash.com/photo-1537691237913-3d0e4d444de4?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            desc: "The right scent can transform a space, creating a relaxing atmosphere, boosting mood, and leaving a lasting impression on anyone who visits. ",
+            url: "https://nurserylive.com/blogs/top-10-plants/top-10-fragrant-flowering-plants-for-ultimate-garden-bliss"
         },
         {
-            title: "Top 10 Plants to Bring Money And Prosperity",
-            date: "18 Mar 2021",
-            image: "https://via.placeholder.com/600x400?text=Lucky+Plants",
-            desc: "Learn about the top 10 plants believed to bring prosperity and positive vibes into your home.",
-            url: "https://nurserylive.com/blogs/top-10-plants/top-10-plants-to-bring-money-and-prosperity-in-your-life"
+            title: "The Mighty Tulsi Plant: Unveiling Its Healing Powers and Growing Tips",
+            date: "June 26, 2023",
+            image: "https://images.unsplash.com/photo-1724913172641-e528a0cc9ec6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fHw%3D",
+            desc: "In a world that’s increasingly reliant on modern medicine, the ancient wisdom of herbs is often overlooked.But fear not! One herb that is absolutely deserving of the spotlight is the mighty Tulsi plant.",
+            url: "https://nurserylive.com/blogs/top-10-plants/the-mighty-tulsi-plant-unveiling-its-healing-powers-and-growing-tips"
         },
         {
-            title: "Top 10 Plants to Beautify Your Terrace Garden",
-            date: "– Apr 2021",
-            tag: "Terrace Garden",
-            image: "https://via.placeholder.com/600x400?text=Terrace+Garden",
-            desc: "These top 10 selections will transform your terrace into a vibrant, serene oasis.",
-            url: "https://nurserylive.com/blogs/top-10-plants/top-10-plants-to-beautify-your-terrace-garden"
+            title: "Survivors of the Arid: Exploring Drought Tolerant Plants!",
+            date: "May 27, 2023",
+            image: "https://images.unsplash.com/photo-1651312166126-151f9f6810cb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZHJvdWdodCUyMHRvbGVyYW50JTIwcGxhbnR8ZW58MHwwfDB8fHww",
+            desc: "Ah, the world of drought- tolerant plant, where resilience meets style, and survival is as trendy as ever! Brace yourself for a witty, fact-filled journey into the realm of these water-saving superheroes.",
+            url: "https://nurserylive.com/blogs/top-10-plants/survivors-of-the-arid-exploring-drought-tolerant-plants"
         },
         {
-            title: "Top 10 Plants for Your Living Room Decor",
-            date: "– 2021",
-            tag: "Home Decor",
-            image: "https://via.placeholder.com/600x400?text=Living+Room+Plants",
-            desc: "Air-purifying, low-maintenance plants perfect for adding life to your living room interiors.",
-            url: "https://nurserylive.com/blogs/top-10-plants/top-10-plants-for-your-living-room-decor"
+            title: "Top 10 Indoor Flowering Plants for Add Colors In Your Interiors",
+            date: "March 30, 202",
+            image: "https://plus.unsplash.com/premium_photo-1664391914821-29ee0b3d98ee?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8aW5kb29yJTIwZmxvd2VyJTIwdmFzZXN8ZW58MHwwfDB8fHww",
+            desc: "A green ambience fills your home with positivity and tranquillity. And who doesn't like that? The fresh fragrance of grass and flowers are soothing to the eyes and refreshing to the mind. ",
+            url: "https://nurserylive.com/blogs/top-10-plants/top-10-indoor-flowering-plants-for-add-colors-in-your-interiors"
         },
     ];
 
@@ -54,7 +52,7 @@ const BlogPage = () => {
             <div className="flex flex-col md:flex-row w-full bg-green-50 h-96">
                 <div className="md:w-1/2">
                     <img
-                        src="https://imgs.search.brave.com/B4aZ9b8OcEQ0NhS-2r2ALGAEeq9_HGZuUAD4Wg7zgYc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/YmhnLmNvbS90aG1i/L3FxdXFkVzl2cTZQ/bndiS1lqZEZ5QWlw/a3Z2QT0vMTUwMHgw/L2ZpbHRlcnM6bm9f/dXBzY2FsZSgpOnN0/cmlwX2ljYygpL2Rp/eS1oYW5naW5nLWlu/ZG9vci1wbGFudGVy/LTc4ODEzZDc1MWI3/ODRmODg5YzQyZWI4/NDQwOGQ3YTIwLmpw/Zw"
+                        src="https://images.unsplash.com/photo-1518564747095-d2fbe4b452b7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBvdHRlZCUyMHBsYW50fGVufDB8MHwwfHx8MA%3D%3D"
                         alt="Plant in Hand"
                         className="w-full h-full shadow-sm object-cover"
                     />
